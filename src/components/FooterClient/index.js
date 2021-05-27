70f1ba1b6cbe04579af496171a3db3dd9511676d
Cuong Nguyen <huycuong10081996@gmail.com>
2021-05-27 14:04:29 +0700

    uo

--- src/components/FooterClient/index.js
+++ src/components/FooterClient/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Row, Col } from 'antd';
 
-import './style.module.css';
+import styles from './style.module.css';
 
 const FooterClient = () => {
   return (
     <Row>
       <Col xs={{ span: 24 }}>Footer</Col>
+      <Col span={8} className={styles.footerLogo}>
+        <Row justify="center">
+          <h1 className={styles.specialHeading}>Book Store</h1>
+        </Row>
+      </Col>
+      <Col span={8}></Col>
+      <Col span={8}></Col>s
     </Row>
   );
 };
